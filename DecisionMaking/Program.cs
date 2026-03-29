c7cc15a4cc99534f40272e4d6439614a1d907f72
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-03-29 19:00:08 +0330

    Else If Keyword and Order of Importance

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -9,64 +9,21 @@ int num2 = 6;
 // The default value for a bool is false
 bool isGreater = num1 > num2;
 int age = 18;
+bool isWithParents = true;
 
 // Greate or EQUAL 18
-if(age >= 18)
+// always goes from top to bottom
+if(age >= 13 && isWithParents)
+{
+    Console.WriteLine("Go party in the club with your parents!");
+} else if (age >= 18)
 {
     Console.WriteLine("Go party in the club!");
 }
+else
+{
+    Console.WriteLine("Go party in the kindergarten!");
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bool isRaining = true;
-bool hasUmberella = false;
-
-
-// Logical Operatos  --> && || !
-// AND &&
-// OR ||
-// NOT !
-
-
-//if(isRaining && hasUmberella)
-//{
-//    Console.WriteLine("I'm protected against rain!");    
-//}
-
-
-// Variants of OR statements
-// true || true -> true 
-// true || flase -> true
-// false || true -> true
-// flase || false -> false
-
-// Variants of AND statements
-// true && true -> true 
-// true && flase -> false
-// false && true -> false
-// flase && false -> false
-
-// negation
-//if (isRaining && !hasUmberella)
-//{
-//    Console.WriteLine("I'm getting WET!");
-//}
-
-
-
-Console.WriteLine("Ay OK");
-Console.ReadKey();
+    Console.ReadKey();
--- DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
+++ DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa62e755fd84c5022d080ef8553cbb8519173b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa7db76e92ef94fb498ae5aa84873236f64321e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
