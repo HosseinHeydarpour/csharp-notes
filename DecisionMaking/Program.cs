3aa7db76e92ef94fb498ae5aa84873236f64321e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-03-29 18:44:10 +0330

    Relational operators

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -1,5 +1,37 @@
 ﻿
 // Two different values a bool can have
+
+int num1 = 5;
+int num2 = 6;
+
+
+// relational operator > >=  _  < <=
+// The default value for a bool is false
+bool isGreater = num1 > num2;
+int age = 18;
+
+// Greate or EQUAL 18
+if(age >= 18)
+{
+    Console.WriteLine("Go party in the club!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 bool isRaining = true;
 bool hasUmberella = false;
 
@@ -10,10 +42,10 @@ bool hasUmberella = false;
 // NOT !
 
 
-if(isRaining && hasUmberella)
-{
-    Console.WriteLine("I'm protected against rain!");    
-}
+//if(isRaining && hasUmberella)
+//{
+//    Console.WriteLine("I'm protected against rain!");    
+//}
 
 
 // Variants of OR statements
@@ -29,10 +61,10 @@ if(isRaining && hasUmberella)
 // flase && false -> false
 
 // negation
-if (isRaining && !hasUmberella)
-{
-    Console.WriteLine("I'm getting WET!");
-}
+//if (isRaining && !hasUmberella)
+//{
+//    Console.WriteLine("I'm getting WET!");
+//}
 
 
 
