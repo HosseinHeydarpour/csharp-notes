596f5d3f549d198a56533079497431f82540e8f3
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-03-29 20:13:16 +0330

    Challenge Clubbing or no Clubbing

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Two different values a bool can have
 
 int num1 = 5;
@@ -40,8 +40,10 @@ else
 
 
 
+
 // reset age to 0
 age = 0;
+*/
 
 /*
 
@@ -66,4 +68,33 @@ else
 }
 
 */
-    Console.ReadKey();
+
+Console.WriteLine("Please enter your age: ");
+int age = int.Parse(Console.ReadLine());
+bool isWithParents;
+
+if(age>=18)
+{
+    Console.WriteLine("Go party in the club!");
+} else if (age >= 13)
+{
+    Console.WriteLine("Are you with your parents? Answer with y or n");
+    string isWithParentsString = Console.ReadLine();
+    if(isWithParentsString == "y")
+    {
+        Console.WriteLine("Go party in the club with your parents!");
+    } else Console.WriteLine("No Party For You!");
+}
+else
+{
+    Console.WriteLine("Go party in the kindergarten!");
+}
+
+
+
+
+
+
+
+
+Console.ReadKey();
--- DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
+++ DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5357eda129c6dac3aa22a4e6af4031fb18c076f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5892830680b8305499d3bdd3b63505a7d9c0d6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
