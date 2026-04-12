adbb161b20c3c9926c3b5845bbc37396fdef6b3a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-12 08:38:45 +0330

    Decrementing and Modulo Operator

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -13,8 +13,42 @@ Console.WriteLine("Num is {0}", ++num);
 
 // increment num after use | post incrementing
 Console.WriteLine("Num is {0}", num++);
-
 Console.WriteLine("Num is {0}", num);
+
+Console.WriteLine("---- Dec ----");
+
+// Decrementing
+num--;
+Console.WriteLine("Num is {0}",num);
+
+// Decrement num before use | pre incrementing
+Console.WriteLine("Num is {0}", --num);
+
+// Decrement num after use | post incrementing
+Console.WriteLine("Num is {0}", num--);
+Console.WriteLine("Num is {0}", num);
+
+Console.WriteLine("--------");
+num += 30;
+Console.WriteLine("Num is {0}", num);
+num -= 10;
+Console.WriteLine("Num is {0}", num);
+
+num *= 10;
+Console.WriteLine("Num is {0}", num);
+
+num /= 20;
+Console.WriteLine("Num is {0}", num);
+
+Console.WriteLine("--------");
+int num1 = 10;
+int num2 = 3;
+
+// Modulo Operator
+int result = num1 % num2;
+// 10/3 = 3 remainder 1
+Console.WriteLine(result);
+
 
 
 Console.ReadKey();
--- DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
+++ DecisionMaking/obj/Debug/net8.0/DecisionMaking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965558e5f13550d45db38067568e0530a33bc815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac1ae2d49216357b17730bbb138f0f88a0b8614")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecisionMaking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
