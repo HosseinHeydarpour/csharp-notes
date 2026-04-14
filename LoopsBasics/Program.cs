5643464fd4fc7b39ee106e0e06b0563d7e74a3c9
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-14 12:15:08 +0330

    Simple Array

--- LoopsBasics/Program.cs
+++ LoopsBasics/Program.cs
@@ -1,18 +1,24 @@
-﻿
-for (int i = 0; i < 4; i++)
-{
-    Console.WriteLine(i);
-    if(i==2)
-    {
-        //Console.WriteLine("I have had enough!");
-        // It gets out of the for loop
-        //break;
+﻿int num1 = 1;
+int num2 = 2;
+int num3 = 3;
+int num4 = 4;
+int num5 = 5;
 
-        continue;
-    }
-    Console.WriteLine(i);
-}
+// Declare an array
+int[] myIntArray = new int[5];
 
+// Assign values to the array
+myIntArray[0] = 5;
+myIntArray[1] = 12;
+myIntArray[2] = 13;
+myIntArray[3] = 14;
+myIntArray[4] = 15;
+//myIntArray[5] = 16; not in array | throws exeption
+
+Console.WriteLine(myIntArray[3]);
+
+// Indexes [0][1][2][3][4]
+// Content [5][12][13][14][15]
 
 
 // DO NOT DELETE
--- LoopsBasics/obj/Debug/net8.0/LoopsBasics.AssemblyInfo.cs
+++ LoopsBasics/obj/Debug/net8.0/LoopsBasics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoopsBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e40ee740545ee4391e28751c78da9e58e5bffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b612bfaf076e7bb7eade3019cf3d76e7afbc90")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoopsBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoopsBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
