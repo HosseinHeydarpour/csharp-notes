2fb68fe242910305dfccd07fbc0bf4828bdcc9ec
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-22 09:03:40 +0330

    Sort() ing a list

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -4,68 +4,30 @@
     {
         static void Main(string[] args)
         {
-            // Declaring a list and initializing 
-            List<string> colors = 
-            [
-               // Adding items to the list | The simpler way
-               "Red","Blue","Green","Cyan","Red"
-            ];
+            List<int> numbers = new List<int>() {10,5,6,12,34,12,13 };
 
+            Console.WriteLine("Unsorted List");
+            DisplayList(numbers);
 
-            // Adding items to the list | but it can be simpler
-            colors.Add("Purple");
-            //colors.Add("Blue");
-            //colors.Add("Green");
-            //colors.Add("Red");
+            Console.WriteLine("===============");
 
-            Console.WriteLine("Current colors in the colors list: ");
-            foreach (string color in colors)
-            {
-               
-                if (Enum.TryParse<ConsoleColor>(color, true, out var consoleColor)) 
-                {
-                    Console.ForegroundColor = consoleColor;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.White; // fallback color
-                }
-                Console.WriteLine(color);
-            }
+            numbers.Sort();
 
-            // This removes the first occurence of an item in the list | jusr first occurence | if we have red blue red --> blue red
-            // Also returns a bool if removal was successful we get true otherwise we get false
-            Console.ResetColor();
-            bool isDeletingSuccess =  colors.Remove("Red");  // True
-            //Console.WriteLine(isDeletingSuccess);
-            // isDeletingSuccess = colors.Remove("Some color which is not in list"); // False
-            //Console.WriteLine(isDeletingSuccess);
-
-            // Delete all "Red" colors
-            while (isDeletingSuccess) {
-                isDeletingSuccess = colors.Remove("Red");
-            }
-   
-
-            Console.WriteLine("\n=============\n");
-            foreach (string color in colors)
-            {
-
-                if (Enum.TryParse<ConsoleColor>(color, true, out var consoleColor))
-                {
-                    Console.ForegroundColor = consoleColor;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.White; // fallback color
-                }
-                Console.WriteLine(color);
-            }
-
-
+            Console.WriteLine("Sorted List");
+            DisplayList(numbers);
+            
+            
 
 
             Console.ReadLine();
+        }
+
+        static void DisplayList<T>(List<T> list) 
+        {
+            foreach (T item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
+++ ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8341793ed6865174ef51c43a3634355857dc1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df275156706ee18beac3e3b43ca3415fb4967990")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
