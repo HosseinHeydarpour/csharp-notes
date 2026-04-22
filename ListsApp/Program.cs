11857fae84eb7db5e49459ec6c37aa44810ac22e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-22 08:24:15 +0330

    Declare a List, add and read List item

---
+++ ListsApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a list and initializing 
+            List<string> colors = new List<string>();
+
+
+
+            colors.Add("Red");
+            colors.Add("Blue");
+            colors.Add("Pink");
+            colors.Add("Green");
+
+            Console.WriteLine("Current colors in the colors list: ");
+            foreach (string color in colors)
+            {
+               
+                if (Enum.TryParse<ConsoleColor>(color, true, out var consoleColor)) 
+                {
+                    Console.ForegroundColor = consoleColor;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White; // fallback color
+                }
+                Console.WriteLine(color);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
