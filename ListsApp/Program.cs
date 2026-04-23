854ec2b21678c326608bdc06e931e01c784faf50
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-23 10:14:15 +0330

    Using Lists with complex Objects

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -2,107 +2,44 @@
 {
     internal class Program
     {
+        public class Product
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+
+        }
+
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>() {10,5,6,12,12,13,100 };
+            
+            // Declare a list of complex objects with initial values
+            List<Product> products = new List<Product>() {
+             // we can set an object property values right after creating it using this syntax -> new Object() {prop1=x...propN=xn}
+             new Product {Name = "Apple", Price = 0.80 },
+             new Product {Name = "Banana", Price = 1.50 },
+             new Product { Name = "Corn", Price = 15 }
+            
+            };
+
+            // Add items to the list
+            products.Add(new Product { Name= "iPhone 17 Pro", Price= 1200 });
 
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Available Products: \n");
+            Console.ResetColor();
 
-            /*
-             *  In C#, a delegate is like a pointer or a reference to a method,
-             *  It allows you to pass methods as arguments to the other methods
-             *  store them in variables, and call them later.
-             *  This is useful when you want your code to be flexible and
-             *  able to handle different behaviors that are not predetermined
-             */
-
-
-            bool hasGreaterThanTwenty = numbers.Any(x => x > 200);
-            if (hasGreaterThanTwenty) {
-                Console.WriteLine("There are large numbers in the number list");
-            } else
+            // iterate through the list
+            foreach (Product product in products)
             {
-                Console.WriteLine("There are no large numbers in the number list");
+                Console.WriteLine($"Product Name: {product.Name} for {product.Price} \n");
             }
-            Console.WriteLine("\n===============");
 
-
-
-
-
-            // Define a predicate to check if a number is greater than or equal 10
-            // A Predicate is just a function that takes one value (here: an int)
-            // and returns TRUE or FALSE. It's used to test/check something.
-            //
-            // In this case:
-            // "isGreaterThan10" will return true if x is 10 or more,
-            // and false if x is less than 10.
-            // You can assign a lambda expression to the delegate or a method
-            //Predicate<int> isGreaterThan10 = x => x >= 10; // simplified method of IsGreaterThanTen
-            Predicate<int> isGreaterThan10 = IsGreaterThanTen; // This works too | also we do not call the method
-
-
-
-            // This will return a list of numbers that are 10 or higher
-            List<int> biggerThanEqualTen = numbers.FindAll(isGreaterThan10);
-
-
-            Console.WriteLine("10 or hihger numbers");
-
-
-            // All numbers ten or higher in numbers list
-            DisplayList(biggerThanEqualTen);
-            Console.WriteLine("===============");
-
-
-
-
-            Console.WriteLine("Unsorted List");
-            DisplayList(numbers);
-
-            Console.WriteLine("===============");
-
-            numbers.Sort();
-
-            Console.WriteLine("Sorted List");
-            DisplayList(numbers);
-            
-            
 
 
             Console.ReadLine();
         }
 
-        // A lambda expression consists of 2 Parts
-        // 1. Parameters
-        // 2. Expression or Statement Block
-
-        // Parameters are written on the left side of =>
-        // (this symbol is read as "goes to" or "becomes")
-        // The expression or action to perform is on the right side
-
-        // This reads as:
-        // " Take an input x and turn it into x multiplied by x. "
-        // x => x * x;
-        // Lambda expression : =>
-
-        // Equivalant method for --> x => x * x;
-        static int Squaring(int num) 
-        { 
-            return num*num;
-        }
-
-        public static bool IsGreaterThanTen(int num) {
-            return num > 10;
-        }
-
-
-        static void DisplayList<T>(List<T> list) 
-        {
-            foreach (T item in list)
-            {
-                Console.WriteLine(item);
-            }
-        }
     }
 }
--- ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
+++ ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b415e3815844c4a7a22cbc109ec2155f424f4bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ede5f7911123e5187b9e95f1d5802e387e08284")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
