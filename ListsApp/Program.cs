d77a70c8e59955892af3176bd16fc248cc372a8e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-29 16:58:09 +0330

    Dictionaries Part 2 - Updating, Removing entries and Iterating through Dictionar

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -18,16 +18,31 @@ namespace ListsApp
                 Dictionary<int,string> employees = new Dictionary<int,string>();
 
 
-                // Adding items to the Dictionary
-                employees.Add(101, "John Doe");
-                employees.Add(102, "Bob Smith");
-                employees.Add(103, "Arthur Morgan");
+            // Adding items to the Dictionary
+            employees.Add(101, "John Doe");
+            employees.Add(102, "Bob Smith");
+            employees.Add(103, "Arthur Morgan");
+            employees.Add(104, "Megan Fox");
+            employees.Add(105, "Jon Snow");
+            employees.Add(106, "Arya Stark");
 
             // we can treat a dict just like an array to access a value
             string name = employees[101];
             Console.WriteLine(name);
 
-                Console.ReadLine();
+            // Update data in a dictionary
+            employees[102] = "Bob Dylan";
+
+            // Remove an item from a dictionary
+            employees.Remove(101);
+
+            // Iterating over a Dictionary
+            foreach (KeyValuePair<int,string> employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.Key}, Name: {employee.Value}");
+            }
+
+            Console.ReadLine();
         }
 
 
--- ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
+++ ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19920e5b69d65b460a4e8476c86c92b3b9d74ec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56093f0d8248a47c50220cbaa79b8c12b9defae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
