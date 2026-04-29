0b42eef9981f721269df81192b931af99c7fb5f0
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-29 17:11:01 +0330

    Gracefully Adding and Updating Dictionaries

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -25,6 +25,7 @@ namespace ListsApp
             employees.Add(104, "Megan Fox");
             employees.Add(105, "Jon Snow");
             employees.Add(106, "Arya Stark");
+          
 
             // we can treat a dict just like an array to access a value
             string name = employees[101];
@@ -34,13 +35,49 @@ namespace ListsApp
             employees[102] = "Bob Dylan";
 
             // Remove an item from a dictionary
-            employees.Remove(101);
+            //employees.Remove(106);
+
+            // This will give us an error because we cannot add an item with the same key
+            //employees.Add(104, "Ned Stark");
+
+            // But this is only updating
+            //employees[104] = "Ned Stark";
+
+            // This is how you can handle duplicates
+            //if (!employees.ContainsKey(104))
+            //{
+            //    employees.Add(104, "Ned Stark");
+            //} else
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Error... This key already exists");
+            //    Console.ResetColor();
+            //}
+
+            /* This is just a workaround
+            int counter = 101;
+            while (employees.ContainsKey(counter))
+            {
+                counter++;
+            }
+            employees.Add(counter, "Jon Snow");
+            */
+
+            // TryAdd here returns a bool | we can use it
+            bool added = employees.TryAdd(102, "Mike Joe");
+            if (!added) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error... This Employee id already exists");
+                Console.ResetColor();
+            }
+
+
 
             // Iterating over a Dictionary
-            foreach (KeyValuePair<int,string> employee in employees)
-            {
-                Console.WriteLine($"ID: {employee.Key}, Name: {employee.Value}");
-            }
+            foreach (KeyValuePair<int, string> employee in employees)
+                {
+                    Console.WriteLine($"ID: {employee.Key}, Name: {employee.Value}");
+                }
 
             Console.ReadLine();
         }
--- ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
+++ ListsApp/obj/Debug/net8.0/ListsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56093f0d8248a47c50220cbaa79b8c12b9defae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77a70c8e59955892af3176bd16fc248cc372a8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
