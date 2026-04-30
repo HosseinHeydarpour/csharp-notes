5ed59ed2abfcb38e69fdbea4ea6564f5d08d09ec
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-04-30 09:28:57 +0330

    Inheritance Base Class vs Derived Class

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -1,10 +1,71 @@
-﻿namespace InheritanceApp
+﻿using System.Reflection.Emit;
+
+namespace InheritanceApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Dog myDog = new Dog();
+
+            // This Eat method comes from the Animal class
+            myDog.Eat();
+            // This method comes from the Dog class 
+            myDog.Bark();
+            
+
+            Console.ReadKey();
         }
     }
+
+
+    // Base Class (Parent Class - Super Class)
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Eating...");
+            Console.ResetColor();
+        }
+    }
+
+    /*
+        INHERITANCE IN C#
+
+        Class Animal { }
+            ↓  (is inherited by)
+            Class Dog : Animal { }
+
+        Explanation:
+            The class Dog inherits from the class Animal using the ":" symbol.
+            This means Dog automatically gains access to all public and protected
+            methods and properties of Animal.
+
+        By inheriting Animal, Dog can:
+            - Use existing Animal methods
+            - Add its own methods and properties
+            - Override Animal methods (if they are virtual)
+    */
+
+    // Derived Class(Child Class or Subclass):
+    // the class that inherits the members of the base class.
+    class Dog : Animal
+    {
+
+        public void Bark()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("WOOF! WOOF!");
+            Console.ResetColor();
+        }
+
+    }
+
+    
+
+
+
+
 }
