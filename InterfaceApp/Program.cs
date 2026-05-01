1e033d12cd5515eaaf261a47305c20f87f2a741d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-01 09:19:53 +0330

    Creating a Folder on Your PC and Logging text there

--- InterfaceApp/Program.cs
+++ InterfaceApp/Program.cs
@@ -8,8 +8,27 @@ namespace InterfaceApp
         static void Main(string[] args)
         {
 
+            // In C# strings, the backslash '\' is an escape character used to represent special characters 
+            // like '\n' (newline) or '\t' (tab). Because of this, a single '\' cannot be written directly 
+            // inside a normal string literal. To represent an actual backslash character, it must be escaped 
+            // by writing it twice: '\\'. Therefore "C:\\Logs" represents the path C:\Logs.
+            //string directoryPath = "C:\\Logs";
+
+            // The @ symbol creates a *verbatim string literal* in C#. In verbatim strings, the backslash '\' 
+            // is not treated as an escape character, so you can write file paths with single backslashes 
+            // (e.g., C:\Logs) without needing to escape them. This makes it easier and more readable for paths.
+            string directoryPath = @"C:\Logs";
+            string message = "This is a log entry";
+
+            if (!Directory.Exists(directoryPath)) 
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string filePath = Path.Combine(directoryPath, "log.txt");
+
             // everytime we run this code this will add hello word to the text file
-            File.AppendAllText("log.text","Hello World"+ "\n");
+            File.AppendAllText(filePath, message + "\n");
            
           
             Console.ReadKey();
--- InterfaceApp/obj/Debug/net8.0/InterfaceApp.AssemblyInfo.cs
+++ InterfaceApp/obj/Debug/net8.0/InterfaceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfaceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce4bd57c3038d360e2a7aca1bab90085c6d0b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497a2543842e2b9dfcd329f98b4a25a1411946b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfaceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfaceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
