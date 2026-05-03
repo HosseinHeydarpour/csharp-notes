923d9e00cb73f2cf1d0a3fb0cf7f0caebaa96a97
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-03 19:22:21 +0330

    Simple delegates example

--- EventsAndDelegatesApp/Program.cs
+++ EventsAndDelegatesApp/Program.cs
@@ -1,12 +1,21 @@
 ﻿namespace EventsAndDelegatesApp
 {
 
-    // 1. Declaration:
-    // This says: "I can hold any method that returns void and takes a string."
-    // You can also declare a delegate outside of any class so it will be easily
-    // accessible by other classes without the need to create an instance object of another class
-    public delegate void Notify(string message);
+    public delegate void LogHandler(string message);
 
+    public class Logger
+    {
+        public void LogToConsole(string message)
+        {
+            Console.WriteLine("Console Log: " + message);
+        }
+
+        public void LogToFile(string message) 
+        {
+            Console.WriteLine("File log: " + message);
+        }
+
+    }
     internal class Program
     {
 
@@ -14,30 +23,18 @@
 
         static void Main(string[] args)
         {
-            // Delegates define a method signature
-            // and any method assigned to a adelegate must match this signature.
 
+            Logger logger = new Logger();
+            LogHandler logHandler = logger.LogToConsole;
+            logHandler("Logging to the console");
 
-
-
-            // 2. Instantiation: 
-            Notify notifyDelegate = ShowMessage;
-
-            // old school way 
-            //Notify notifyDelegate = new Notify(ShowMessage);
-
-
-
-            // 3. Invocation: 
-            notifyDelegate("Hello, Delegates!");
-
+            logHandler = logger.LogToFile;
+            logHandler("Log some stuff in file");
 
 
             Console.ReadKey();
         }
 
-        static void ShowMessage(string message) {
-            Console.WriteLine(message);
-        }
+        
     }
 }
--- EventsAndDelegatesApp/obj/Debug/net8.0/EventsAndDelegatesApp.AssemblyInfo.cs
+++ EventsAndDelegatesApp/obj/Debug/net8.0/EventsAndDelegatesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsAndDelegatesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae53018801a216a849c4c49266b93e04169c502")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsAndDelegatesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsAndDelegatesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
