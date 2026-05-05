178c24781a8c6928fc33f4a30c092897f40c1d5d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-05 07:54:39 +0330

    Using dots and wildcards, Regex Matching, and Quantifiers

--- RegexApp/obj/Release/net8.0/RegexApp.AssemblyInfo.cs
+++ RegexApp/obj/Release/net8.0/RegexApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f430177b90d727ec13e0a986b5b61dc33172a0ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
