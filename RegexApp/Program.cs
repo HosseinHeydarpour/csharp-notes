f430177b90d727ec13e0a986b5b61dc33172a0ad
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-05 07:37:49 +0330

    What is Regex

---
+++ RegexApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace RegexApp
+{
+    // What are Regular Expressions
+    // A sequence of characters that define a search pattern.
+    // These patterns are used to match and manipulate strings based on specific criteria,
+    // allowing for powerful text processing and data validation.
+    // Example breakdown of a regular expression:
+    // /[\w._%±]+@[\w.-]+.[a-zA-Z]{2,4}/
+    // [\w._%±]+ → match any word character, dot, underscore, percent, plus, or hyphen,
+    // as many times as possible
+    // @ → match the @ symbol
+    // [\w.-]+ → match any word character, dot, or hyphen
+    // . → match a literal dot
+    // [a-zA-Z]{2,4} → match upper- or lowercase letters, at least 2 times but no more than 4 times
+
+
+    // Why Use Regular Expressions?
+    // Validate inputs (emails, URLs, etc.).
+    // Ensure that user-entered data adheres to the required format,
+    // such as checking if an email address or URL is valid.
+    //-------
+    // Search and replace text.
+    // Efficiently find and replace specific patterns within large texts,
+    // streamlining data cleaning and modification tasks.
+    //-------
+    // Extract specific patterns.
+    // Identify and extract meaningful data, such as phone numbers or dates,
+    // from unstructured text for further processing.
+    //-------
+
+    // Regex in Various Languages
+    // Regex is a universal tool used across many programming languages,
+    // including, but not limited to,
+    // C#, C++, Java, JavaScript, and Python.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
