9c6c10b2ede10e6fc0cab67abb8b61d9618a4a8d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-06 15:24:59 +0330

    Fixed, auto and relative sizing

--- WpfDemo/obj/Debug/net8.0-windows/WpfDemo.AssemblyInfo.cs
+++ WpfDemo/obj/Debug/net8.0-windows/WpfDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30cb5f81c868a808a25dadf91c6d930b3774b96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3890d673bd492c9a7cbfa67da923d97751055589")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
