56874e1bd4d7d5e78d224cd13799ae18e87458d5
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-07 09:26:34 +0330

    Setting up the data to bind

--- WpfDemo/MainWindow.xaml.cs
+++ WpfDemo/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfDemo.Data;
 
 namespace WpfDemo
 {
@@ -16,11 +17,22 @@ namespace WpfDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Person Person = new Person
+        {
+            Name = "Hossein",
+            Age = 28
+        };
+
+
         public MainWindow()
         {
             InitializeComponent();
 
-            
+            // Sets the DataContext for the window, which acts as the default data source
+            // for all data bindings in this view. UI elements can bind to properties
+            // of the Person object (e.g., {Binding Name}) without explicitly referencing it.
+            this.DataContext = Person;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
--- WpfDemo/obj/Release/net8.0-windows/WpfDemo.AssemblyInfo.cs
+++ WpfDemo/obj/Release/net8.0-windows/WpfDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a23c135395d7fc19418987a8d931b8ff27ca6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d41418a1d226080ab33a592072b9efec0b7b818")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
