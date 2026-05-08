8307d4fc1572c22596d96c09ae7ab5f56558f91c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-08 10:09:03 +0330

    Adding the password box

--- InvoiceManagement/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ InvoiceManagement/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace InvoiceManagement {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/InvoiceManagement;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/InvoiceManagement;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
