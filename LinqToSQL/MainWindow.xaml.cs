6367d8d6d57def5afcaa2923e960404da81476cb
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-05-22 15:07:02 +0330

    Setting up the project for LinqToSQL

--- LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace LinqToSQL
 {
@@ -20,9 +21,18 @@ namespace LinqToSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        LinqToDataClassesDataContext dataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.LinqDBConnectionString"].ConnectionString;
+
+            dataContext = new LinqToDataClassesDataContext(connectionString);
+
         }
     }
 }
