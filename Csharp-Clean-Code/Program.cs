514e63e2ee7efdb00248098c13450e2ce0c7e263
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-21 10:34:37 +0330

    Code TODOs

--- Csharp-Clean-Code/Program.cs
+++ Csharp-Clean-Code/Program.cs
@@ -24,48 +24,9 @@ namespace Csharp_Clean_Code
     public class  MathUtils
     {
 
-        // Comment (inline comment)
-
-        /*
-         * Multiple 
-         * Line
-         * Comment
-        */
-
-
-
-        // Bad: Calculates the factorial number | it is bad because it is so obvious! | Not what but why!
-        // Using a recursive approach for calculation | bad also we must say why is this happening
-        // We are using a recursive approach here BECAUSE it's more intuitive(GOOD way)
-        public int CalculateFactorial(int number)
-        {
-            if(number <= 1)
-            {
-                return 1; 
-            }
-            else
-            {
-                 return number * CalculateFactorial(number - 1);
-            }
-        }
-
-
-        // Using binary search beacuse we want to Improve the performance for the large datasets
+        // TODO: Implement the binary search algorithm
         public int BinarySearch(int[] sortedArray, int target) 
         {
-            int left = 0;
-            int right = sortedArray.Length-1;
-
-            while (left <= right) 
-            { 
-                int middle = (left + right)/2;
-
-                if (sortedArray[middle] == target) return middle;
-                else if (sortedArray[middle] < target) left = middle + 1;
-                else right = middle - 1;
-            
-            }
-
             return -1;
         }
     }
