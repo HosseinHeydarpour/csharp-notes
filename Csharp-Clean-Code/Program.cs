a4f963d6c9f9142f4605beb7f0e6783818cd7310
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-22 11:29:02 +0330

    KISS Principle (Keep it simple, stupid)

--- Csharp-Clean-Code/Program.cs
+++ Csharp-Clean-Code/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,69 @@ namespace Csharp_Clean_Code
     {
         static void Main(string[] args)
         {
-            DiscountCalculator discountCalculator = new DiscountCalculator();
+           List<Order> orders = new List<Order>
+           {
+               new Order { Id = 1, ProductName = "Laptop", Quantity = 2, Price = 1500 },
+               new Order { Id = 2, ProductName = "Phone", Quantity = 5, Price = 500 }
+           };
 
-            double regularCustomerDiscount = discountCalculator.CalculateDiscount(CustomerType.Regular, 1200);
-            Console.WriteLine($"Regular Customer Discount: {regularCustomerDiscount}$" );
-
-
-            double premiumCustomerDiscount = discountCalculator.CalculateDiscount(CustomerType.Premium, 800);
-            Console.WriteLine($"Premium Customer Discount: {premiumCustomerDiscount}$");
-
-
-            double employeeCustomerDiscount = discountCalculator.CalculateDiscount(CustomerType.Employee,1500);
-            Console.WriteLine($"Employee Customer Discount: {employeeCustomerDiscount}$");
+            OrderPrinter printer = new OrderPrinter();
+            printer.PrintOrderDetails(orders);
 
         }
 
     }
 
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+
+
+    // Implementing KISS(Keep It Simple Stupid) Principle
+    class OrderPrinter
+    {
+        public void PrintOrderDetails(List<Order> orders)
+        {
+            double totalPrice = 0;
+            foreach (var order in orders)
+            {
+              
+                if (order.Quantity > 0 && order.Price > 0)
+                {
+                    double total = order.Quantity * order.Price;
+                    Console.WriteLine("Order ID: "+ order.Id);
+                    Console.WriteLine("Product: "+ order.ProductName);
+                    Console.WriteLine("Quantity: "+ order.Quantity);
+                    Console.WriteLine("Price: " + order.Price);
+                    Console.WriteLine("Total: "+ total);
+                    Console.WriteLine("------------");
+
+                    totalPrice += order.Quantity * order.Price;
+
+                }
+            }
+           
+           
+            //foreach (var order in orders)
+            //{   
+            //    if(order.Quantity>0 && order.Price > 0)
+            //    {
+            //        totalPrice += order.Quantity * order.Price;
+            //    }
+            //}
+
+            Console.WriteLine("Total Price: " + totalPrice);
+
+        }
+    }
 
     
 
