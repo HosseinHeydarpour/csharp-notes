238289592c1d600da1bacf324dd7507990d5c8dd
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-23 10:39:37 +0330

    Exception handling

--- Csharp-Clean-Code/Program.cs
+++ Csharp-Clean-Code/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Csharp_Clean_Code
 {
@@ -11,71 +12,48 @@ namespace Csharp_Clean_Code
     {
         static void Main(string[] args)
         {
-           List<Order> orders = new List<Order>
-           {
-               new Order { Id = 1, ProductName = "Laptop", Quantity = 2, Price = 1500 },
-               new Order { Id = 2, ProductName = "Phone", Quantity = 5, Price = 500 }
-           };
 
-            OrderPrinter printer = new OrderPrinter();
-            printer.PrintOrderDetails(orders);
-
+            ReadFile("c://");
         }
 
-    }
 
-
-    public class Order
-    {
-        public int Id { get; set; }
-
-        public string ProductName { get; set; }
-
-        public int Quantity { get; set; }
-
-        public int Price { get; set; }
-    }
-
-
-    // Implementing KISS(Keep It Simple Stupid) Principle
-    class OrderPrinter
-    {
-        public void PrintOrderDetails(List<Order> orders)
+        public static void ReadFile(string filePath)
         {
-            double totalPrice = 0;
-            foreach (var order in orders)
+
+            try
             {
-              
-                if (order.Quantity > 0 && order.Price > 0)
-                {
-                    double total = order.Quantity * order.Price;
-                    Console.WriteLine("Order ID: "+ order.Id);
-                    Console.WriteLine("Product: "+ order.ProductName);
-                    Console.WriteLine("Quantity: "+ order.Quantity);
-                    Console.WriteLine("Price: " + order.Price);
-                    Console.WriteLine("Total: "+ total);
-                    Console.WriteLine("------------");
-
-                    totalPrice += order.Quantity * order.Price;
-
-                }
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine(content);
             }
-           
-           
-            //foreach (var order in orders)
-            //{   
-            //    if(order.Quantity>0 && order.Price > 0)
-            //    {
-            //        totalPrice += order.Quantity * order.Price;
-            //    }
-            //}
+            // File not found
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Oops! File not found! " + ex.Message);
+            }
+            // Unauhtorized Access
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Oops! You are not allowed to access this file! " + ex.Message);
+            }
+            // Any Other Exception
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops! Something went wrong! " + ex.Message);
+            }
 
-            Console.WriteLine("Total Price: " + totalPrice);
 
+
+
+        
+       
+            
         }
+
+
     }
 
-    
+
+ 
 
 
 }
