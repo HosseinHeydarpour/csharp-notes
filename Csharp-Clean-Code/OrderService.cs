fab6ad1de60ce20d7aa71514fc1871357fb908fb
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-23 10:58:09 +0330

    Single Responsibility Principle (SRP)

---
+++ Csharp-Clean-Code/OrderService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Clean_Code
+{
+
+    // We have multiple responsibilities here
+    // so we create two new classes - and order service class remains as coordinator
+    public class OrderService
+    {
+
+        private List<Order> orders = new List<Order>();
+
+        private OrderLogger orderLogger  =  new OrderLogger();
+        private OrderNotifier orderNotifier = new OrderNotifier();
+
+
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+            orderLogger.LogOrder(order);
+            orderNotifier.NotifyCustomer(order);
+            
+        }
+
+
+    }
+
+    // In the real word create these classes in a new file
+    public class OrderLogger
+    {
+        public void LogOrder(Order order)
+        {
+            // Log the order to a file
+            Console.WriteLine($"Order {order.Id} logged.");
+        }
+    }
+
+
+
+    public class OrderNotifier
+    {
+        public void NotifyCustomer(Order order)
+        {
+            // Send a notification to the customer
+            Console.WriteLine($"Customer notified for order {order.Id}. ");
+        }
+    }
+}
--- Csharp-Clean-Code/Program.cs
+++ Csharp-Clean-Code/Program.cs
@@ -12,42 +12,20 @@ namespace Csharp_Clean_Code
     {
         static void Main(string[] args)
         {
-
-            ReadFile("c://");
-        }
-
-
-        public static void ReadFile(string filePath)
-        {
-
-            try
+            Order order = new Order
             {
-                string content = File.ReadAllText(filePath);
-                Console.WriteLine(content);
-            }
-            // File not found
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine("Oops! File not found! " + ex.Message);
-            }
-            // Unauhtorized Access
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine("Oops! You are not allowed to access this file! " + ex.Message);
-            }
-            // Any Other Exception
-            catch (Exception ex)
-            {
-                Console.WriteLine("Oops! Something went wrong! " + ex.Message);
-            }
+                Id = 1,
+                ProductName = "Test",
+                Quantity = 1,
+                Price = 100,
+            };
 
-
-
-
-        
-       
+            OrderService orderService = new OrderService();
+            orderService.AddOrder(order);
             
         }
+
+
 
 
     }
