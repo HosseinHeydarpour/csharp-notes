ad32e68b93ff8e58ab26f6707b0cd80b88c06a0f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-28 20:11:35 +0330

    Refactoring and Adding Domain

---
+++ Calculator-tdd/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿
+using Xunit;
+using System;
+using Domain;
+
+namespace Calculator_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var calculator = new Calculator();
+            // Testing Condition
+            if(calculator.Sum(2,2) != 4)
+            {
+                throw new Exception("Test failed: 2 + 2 did not equal 4.");
+            }
+        }
+
+
+
+        
+
+
+    }
+}
