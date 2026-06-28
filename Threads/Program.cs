bbfed49485efcdc0ac208e255edaa60f66d9139a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-28 10:28:19 +0330

    Thread Start and End &Completion

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Threads
 {
@@ -20,7 +21,7 @@ namespace Threads
             Console.WriteLine("Hello World 3");
             Thread.Sleep(1000); // Pause our whoel program for 1 second
             Console.WriteLine("Hello World 4");
-            */
+         
 
             new Thread(() =>
             {
@@ -43,6 +44,25 @@ namespace Threads
                 Console.WriteLine("Thread 4");
             }).Start();
 
+            */
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() =>
+            {
+                Console.WriteLine($"Thread Number: {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(5000);
+                taskCompletionSource.TrySetResult(true);
+                Console.WriteLine($"Thread Number: {Thread.CurrentThread.ManagedThreadId} ended");
+            });
+    
+            
+
+            thread.Start();
+            var task = taskCompletionSource.Task.Result;
+            Console.WriteLine("Task was done: {0}",task);
+
+            Console.ReadLine();
         }
     }
 }
