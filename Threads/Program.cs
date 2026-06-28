6559badf999c7592350ad404fc16f8594de6b9c2
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-06-28 10:12:12 +0330

    Threads Basics

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threads
 {
@@ -10,6 +10,39 @@ namespace Threads
     {
         static void Main(string[] args)
         {
+
+            // Excecutes one row after another
+            /*
+            Console.WriteLine("Hello World 1");
+            Thread.Sleep(1000); // Pause our whoel program for 1 second
+            Console.WriteLine("Hello World 2");
+            Thread.Sleep(1000); // Pause our whoel program for 1 second
+            Console.WriteLine("Hello World 3");
+            Thread.Sleep(1000); // Pause our whoel program for 1 second
+            Console.WriteLine("Hello World 4");
+            */
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+
         }
     }
 }
