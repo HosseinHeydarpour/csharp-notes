38eb4a9f53d774667363e6539c9172b888631a9f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-01 11:13:10 +0330

    Translating a Scenario to Test

---
+++ Domain.Tests/Flight.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Tests
+{
+    public class Flight
+    {
+        public int RemainingNumberOfSeats { get; set; }
+
+        
+
+        public Flight(int seatCapicity)
+        {
+            RemainingNumberOfSeats = seatCapicity;
+        }
+
+        public void Book(string v1, int v2)
+        {
+            RemainingNumberOfSeats -=   v2;
+        }
+    }
+}
--- Flight/UnitTest1.cs
+++ Flight/UnitTest1.cs
@@ -1,4 +1,9 @@
-﻿namespace Flight
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Domain.Tests;
+
+namespace FlightTest
 {
     public class UnitTest1
     {
@@ -6,6 +11,13 @@
         public void Test1()
         {
 
+            var flight = new Domain.Tests.Flight(seatCapicity: 3);
+
+            flight.Book("Hossein@gmail.com", 1);
+
+            flight.RemainingNumberOfSeats.Should().Be(2);   
         }
     }
+
+    
 }
