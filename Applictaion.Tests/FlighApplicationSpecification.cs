c0c4ef1a64d16d5de1942b0fe28ded7b36941016
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-02 19:52:11 +0330

    Scenario: Application Layer Booking Part One

--- Applictaion.Tests/FlighApplicationSpecification.cs
+++ Applictaion.Tests/FlighApplicationSpecification.cs
@@ -1,11 +1,56 @@
-﻿namespace Applictaion.Tests
+﻿using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Applictaion.Tests
 {
     public class FlighApplicationSpecification
     {
         [Fact]
         public void Books_flights()
         {
+           var bookingService = new BookingService();
+
+            bookingService.Book(new BookDto());
+
+            bookingService.FindBookings().Should().ContainEquivalentOf(new BookingRm());
+
 
         }
     }
+
+
+  
+
+    public class BookingService
+    {
+
+
+        public void Book(BookDto bookDto) { 
+        
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookingRm> FindBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    // For transfering data
+    public class BookDto
+    {
+
+    }
+
+    // For reading data
+    public class BookingRm
+    {
+
+    }
+
 }
--- Applictaion.Tests/obj/Debug/net10.0/Applictaion.Tests.AssemblyInfo.cs
+++ Applictaion.Tests/obj/Debug/net10.0/Applictaion.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Applictaion.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d062554e65346e8d22a540de516c6dd38d5b134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b683f954e82ceb11ae21ce3af0cf6059bbe3c6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Applictaion.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Applictaion.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
